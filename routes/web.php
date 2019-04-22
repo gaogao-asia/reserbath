df104fad348cf7dff03057d1009198dcbabe3af5
RikuHirose <rikuparkour9@gmail.com>
2019-04-23 01:31:13 +0900

    feature/issue_#3/auth

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@
 $reserbath_domain = env('RESERBATH_DOMAIN');
 Route::domain($reserbath_domain)->group(function () {
   Route::group(['as' => 'www.', 'namespace' => 'Www'], function () {
-    Route::get('/', function () {
-      return view('welcome');
-    });
+    Route::get('/', 'HomeController@index');
   });
 });
+
+Auth::routes();
+
