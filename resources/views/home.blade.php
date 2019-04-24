71a08c936a9cb3a258974a3b288b838458533b86
RikuHirose <rikuparkour9@gmail.com>
2019-04-24 18:32:11 +0900

    UI/issue_#5

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-
-            <a href="{{ route('www.reservations.create') }}">風呂を予約する</a>
+    @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
         </div>
-    </div>
-</div>
+    @endif
+
+    <a href="{{ route('www.reservations.create') }}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">風呂を予約する</a>
 @endsection
--- resources/views/www/reservations/create.blade.php
+++ resources/views/www/reservations/create.blade.php
@@ -1,10 +1,18 @@
-<div>
-    {!! Form::open(['url' => route('www.reservations.store'), 'method' => 'post']) !!}
+@extends('layouts.app')
 
-    <div>
-        {!! Form::label('reserve_time', __('models.reservation.reserve_time')) !!}
-        {!! Form::datetime('reserve_time', $value = null, $attributes = ['id' => 'reserve_time']) !!}
-    </div>
-    {!! Form::submit('予約する') !!}
-    {!! Form::close() !!}
-</div>
+@section('content')
+  <div>
+      <h3>予約の追加</h3>
+      <p class="text-right">
+          <a href="{{ route('www.reservations.index') }}" class="pull-right btn btn-secondary btn-sm active">予約一覧に戻る</a>
+      </p>
+  </div>
+  {!! Form::open(['url' => route('www.reservations.store'), 'method' => 'post']) !!}
+
+  <div class="form-group">
+      {!! Form::label('reserve_time', __('models.reservation.reserve_time')) !!}
+      {!! Form::datetime('reserve_time', $value = null, $attributes = ['id' => 'reserve_time', 'class' => 'form-control', 'placeholder' => 'Enter your resevation bath time!!']) !!}
+  </div>
+  {!! Form::submit('予約する', $attributes =['class' => 'btn btn-primary']) !!}
+  {!! Form::close() !!}
+@endsection
