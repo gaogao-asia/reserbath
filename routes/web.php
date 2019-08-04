12aa79a40a807480b07101b3bf0b81542fc26923
mass-min <koyod111@gmail.com>
2019-08-04 17:07:28 +0900

    refs #28 fix indent and lines

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,15 @@
 $reserbath_domain = env('RESERBATH_DOMAIN');
 Route::domain($reserbath_domain)->group(function () {
     Route::group(['as' => 'www.', 'namespace' => 'Www'], function () {
-        // Route::get('/', 'HomeController@index')->name('index');
         Route::get('{all}', 'HomeController@index')->where('all', '^((?!auth).)*');
-        // Route::get('/{any?}', 'HomeController@index')->where('any', '.*');
-        // Route::resource('reservations', 'ReservationsController');
     });
 });
 
-Route::group(['prefix' => 'auth', 'middleware' => ['guest']], function ()
-{
-  Route::get('login/{provider}', 'Auth\SocialController@redirectToProvider');
-  Route::get('{provider}/callback', 'Auth\SocialController@handleProviderCallback');
-
-
+Route::group(['prefix' => 'auth', 'middleware' => ['guest']], function () {
+    Route::get('login/{provider}', 'Auth\SocialController@redirectToProvider');
+    Route::get('{provider}/callback', 'Auth\SocialController@handleProviderCallback');
 });
 
 Route::group(['prefix' => 'auth'], function () {
-  Auth::routes();
+    Auth::routes();
 });
