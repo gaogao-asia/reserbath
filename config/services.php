46899362db0b27402414c10800e22d73c7289c3e
onukichi <onuonu007@gmail.com>
2019-11-12 00:06:55 +0900

    fix using slack url

--- config/services.php
+++ config/services.php
@@ -24,6 +24,10 @@ return [
         'token' => env('POSTMARK_TOKEN'),
     ],
 
+    'slack' => [
+        'tokyo_house' => env('SLACK_NOTIFICATION_URL'),
+    ],
+
     'ses' => [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
